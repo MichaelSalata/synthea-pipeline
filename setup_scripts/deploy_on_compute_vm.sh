eeaefcbdee113d9553a7c3646cc3fab7e6d37509
Michael Salata <michael.d.salata@gmail.com>
2025-07-20 10:41:04 -0500

    update deploy script to download example_data

--- setup_scripts/deploy_on_compute_vm.sh
+++ setup_scripts/deploy_on_compute_vm.sh
@@ -22,6 +22,14 @@ cp -f ./terraform.tfvars ./${github_project_name}/terraform/terraform.tfvars
 cp -f ./.env ./${github_project_name}/airflow-gcp/.env
 bash update_vm_env.sh
 
+echo "downloading Synthea sample data..."
+wget -O synthea_sample_data.zip https://synthetichealth.github.io/synthea-sample-data/downloads/latest/synthea_sample_data_csv_latest.zip
+
+echo "creating example_data directory and extracting files..."
+mkdir -p ./${github_project_name}/airflow-gcp/example_data/
+unzip -o synthea_sample_data.zip -d ./${github_project_name}/airflow-gcp/example_data/
+rm synthea_sample_data.zip
+
 cd ./${github_project_name}/airflow-gcp
 mkdir ./logs ./plugins ./config
 DOCKER_BUILDKIT=1 docker compose build
